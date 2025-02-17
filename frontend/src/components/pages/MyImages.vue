5e9346362ef9df2786ac66ce3ac684f89e01b723
Benja Netan <netanyahubenjamin70@gmail.com>
2025-02-17 18:30:00 +0300

    refactor DefaultLayout and update routing; add upload functionality to Home and MyImages pages

--- frontend/src/components/pages/MyImages.vue
+++ frontend/src/components/pages/MyImages.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>My Images</h1> 
+  <div>
+    <header class="bg-white shadow-sm">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">My Images</h1>
+      </div>
+    </header>
+    <main>
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"></div>
+    </main>
+  </div>
 </template>
 
 <style></style>
